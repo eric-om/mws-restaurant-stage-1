2ea7486a068592ec887d1de6ff3f4da5c3098d94
Eric Om <om@berkeley.edu>
2018-11-08 21:42:08 -0800

    fixed service worker

--- sw.js
+++ sw.js
@@ -1,47 +1,37 @@
-// sourced from https://developers.google.com/web/ilt/pwa/lab-caching-files-with-service-worker
+// reference: https://alexandroperez.github.io/mws-walkthrough/?1.23.registering-service-worker-and-caching-static-assets
+
+const appName = "restaurant-reviews"
+const staticCacheName = appName + "-v1.0";
+const contentImgsCache = appName + "-images";
+
+var allCaches = [
+  staticCacheName,
+  contentImgsCache
+];
 
 const filesToCache = [
   '/index.html',
   '/restaurant.html',
   '/css/styles.css',
-  '/js/api_helper.js',
+  '/js/db_helper.js',
   '/js/main.js',
   '/js/restaurant_info.js',
   '/js/dbhelper.js',
   '/data/restaurants.json',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg'
 ];
 
-const staticCacheName = 'pages-cache-v1';
-
-self.addEventListener('install', event => {
-  console.log('Attempting to install service worker and cache static assets');
+/** At Service Worker Activation, Delete previous caches, if any */
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith(appName) &&
+                 !allCaches.includes(cacheName);
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
